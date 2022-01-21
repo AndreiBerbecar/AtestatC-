33b05ad3e90c7bc3f29071a516e2481777aeee94
AndreiBerbecar <51360153+AndreiBerbecar@users.noreply.github.com>
2022-01-21 08:43:29 +0200

    Add files via upload

---
+++ Atestat C#/Atestat/Atestat/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Atestat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pB1
+            // 
+            this.pB1.Image = ((System.Drawing.Image)(resources.GetObject("pB1.Image")));
+            this.pB1.Location = new System.Drawing.Point(106, 24);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(97, 117);
+            this.pB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB1.TabIndex = 0;
+            this.pB1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BEST HOME";
+            // 
+            // pb2
+            // 
+            this.pb2.Image = ((System.Drawing.Image)(resources.GetObject("pb2.Image")));
+            this.pb2.Location = new System.Drawing.Point(52, 236);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(30, 30);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb2.TabIndex = 2;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.pnl1.Location = new System.Drawing.Point(52, 265);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(236, 1);
+            this.pnl1.TabIndex = 3;
+            // 
+            // pb3
+            // 
+            this.pb3.Image = ((System.Drawing.Image)(resources.GetObject("pb3.Image")));
+            this.pb3.Location = new System.Drawing.Point(52, 313);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(30, 30);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb3.TabIndex = 2;
+            this.pb3.TabStop = false;
+            this.pb3.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.panel1.Location = new System.Drawing.Point(52, 342);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(236, 1);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnLogIn.FlatAppearance.BorderSize = 0;
+            this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogIn.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogIn.ForeColor = System.Drawing.Color.White;
+            this.btnLogIn.Location = new System.Drawing.Point(52, 393);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(236, 34);
+            this.btnLogIn.TabIndex = 4;
+            this.btnLogIn.Text = "LOG IN";
+            this.btnLogIn.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(199, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Clear Fields";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(142, 461);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(41, 20);
+            this.lbl3.TabIndex = 5;
+            this.lbl3.Text = "Exit";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.txtUserName.Location = new System.Drawing.Point(88, 241);
+            this.txtUserName.Multiline = true;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(200, 24);
+            this.txtUserName.TabIndex = 6;
+            // 
+            // textPassword
+            // 
+            this.textPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPassword.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.textPassword.Location = new System.Drawing.Point(88, 316);
+            this.textPassword.Multiline = true;
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(200, 24);
+            this.textPassword.TabIndex = 6;
+            this.textPassword.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(333, 510);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pnl1);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pB1);
+            this.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pB1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox textPassword;
+    }
+}
+
